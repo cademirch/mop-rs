05fbfadefc7aed42d23505c34f4528b4d4b9491b
Cade Mirchandani <cmirchan@ucsc.edu>
2024-09-06 16:25:05 -0700

    add ci tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,15 +1,46 @@
-// use assert_cmd::prelude::*; // Add methods on commands
-// use predicates::prelude::*; // Used for writing assertions
-// use std::process::Command; // Run programs
+use assert_cmd::prelude::*; 
+use std::fs;
+use std::io::Read;
+use std::process::Command;
+use std::path::Path;
 
-// #[test]
-// fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-//     let mut cmd = Command::cargo_bin("moprs")?;
+#[test]
+fn test_moprs_output() -> Result<(), Box<dyn std::error::Error>> {
+    
+    let mut cmd = Command::cargo_bin("moprs")?;
+    
+    
+    cmd.arg("--d4").arg("tests/data/test.per-base.d4")
+        .arg("-m").arg("3")
+        .arg("-M").arg("20")
+        .arg("-d").arg("1.0")
+        .arg("-u").arg("10");
 
-//     cmd.arg("foobar").arg("test/file/doesnt/exist");
-//     cmd.assert()
-//         .failure()
-//         .stderr(predicate::str::contains("could not read file"));
+    
+    let output_file = "test.bed";
 
-//     Ok(())
-// }
+    
+    let output = cmd.output()?;
+    
+    
+    fs::write(output_file, &output.stdout)?;
+
+    
+    let mut generated_output = String::new();
+    fs::File::open(output_file)?
+        .read_to_string(&mut generated_output)?;
+
+    
+    let expected_output_path = Path::new("tests/data/truth.bed");
+    let mut expected_output = String::new();
+    fs::File::open(expected_output_path)?
+        .read_to_string(&mut expected_output)?;
+
+    
+    assert_eq!(generated_output, expected_output);
+
+    // Clean up by removing the generated output file
+    fs::remove_file(output_file)?;
+
+    Ok(())
+}
